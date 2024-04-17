17d7c2582a16c6a8e3bb638fa6b00cc9b76bfefa
Sergi Samperi <sergi.sl.87@gmail.com>
2024-04-17 15:50:39 +0200

    Crear metodo para lista

--- ejercicio/src/main/java/com/uf4/ejercicio/Controlador.java
+++ ejercicio/src/main/java/com/uf4/ejercicio/Controlador.java
@@ -1,6 +1,9 @@
 package com.uf4.ejercicio;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class Controlador {
     @GetMapping("/saludar")
     public String saludo() {
         return "Hola que tal";
+    }
+
+    @PostMapping("/lista")
+    public ArrayList<String> lista() {
+        return new ArrayList<String>();
     }
 }
